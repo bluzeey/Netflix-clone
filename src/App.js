9c4a9d433ae0bb38a8b8fd09cf60e2eaea5d616b
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-03 22:12:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
-import { useAuthListener } from './hooks';
+import  useAuthListener  from './hooks/use-auth-listener';
 
 export function App() {
   const { user } = useAuthListener();
